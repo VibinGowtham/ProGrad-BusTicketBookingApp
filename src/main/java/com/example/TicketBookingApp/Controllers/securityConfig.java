ec9bb22a7a2b345da38a701bafd63de3b3b13df7
unknown <vibingowtham.cs18@bitsathy.ac.in>
2022-04-01 09:39:03 +0530

    Removed an unwanted comment

--- src/main/java/com/example/TicketBookingApp/Controllers/securityConfig.java
+++ src/main/java/com/example/TicketBookingApp/Controllers/securityConfig.java
@@ -39,10 +39,4 @@ public class securityConfig extends WebSecurityConfigurerAdapter{
 		return super.authenticationManager();
 	}
 	
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests()
-//		.antMatchers("/admin").hasRole("ADMIN")
-//		.antMatchers("/user").hasAnyRole("USER","ADMIN")
-//		.and().formLogin();
-//	}
 }
